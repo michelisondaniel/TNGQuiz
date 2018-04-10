93c49123a2b7b8609280d8a575ce31e7aae60ad4
michelisondaniel <michelison.daniel@gmail.com>
2018-04-10 02:20:53 -0700

    added gif images to the quiz on the mainActivity page and resultsActivity also added a package to the build.gradle(Module) to enable the GifTextView widget.

--- app/src/main/java/net/michelison/tngquiz/MainActivity.java
+++ app/src/main/java/net/michelison/tngquiz/MainActivity.java
@@ -1,23 +1,35 @@
 package net.michelison.tngquiz;
 
+// images for this quiz were acquired from giphy.com and images also from the
+// website from which I recrafted most of these questions for my own needs
+// https://www.thequiz.com/only-a-real-star-trek-fan-can-get-100-on-this-tng-quiz/
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import pl.droidsonroids.gif.GifTextView;
+
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     Button startButton;
+    GifTextView spockGifView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spockGifView = (GifTextView) findViewById(R.id.spockGifView);
         startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
+
+        spockGifView.setBackgroundResource(R.drawable.dwnspockdog);
+
 
         }
 
